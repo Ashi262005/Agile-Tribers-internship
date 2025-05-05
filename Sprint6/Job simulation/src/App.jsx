f4e173521b7f575d78cf75c955b3f0cd5b1069f7
Ashika A L <ashikaalexander4@gmail.com>
2025-05-05 23:12:59 +0530

    Add files via upload

---
+++ Sprint6/Job simulation/src/App.jsx
@@ -0,0 +1,25 @@
+// import { Header } from "./components/Header";
+// import { Card } from "./components/Card";
+// import { QRcode } from "./components/QRcode";
+// import { MUI } from "./components/MUI";
+import { Task } from "./components/Task";
+// import './components/MUI.css';
+// import { Student } from "./components/Student";
+// import './components/QRcode.css';
+// import './components/Card.css';
+// import './components/Header.css';
+// import './components/Student.css';
+// import './Weatherapp/Weather.css';
+// import { Weather } from './Weatherapp/Weather';
+export const App = () => {
+  return (
+    // <QRcode/>
+    // <Card/>
+    // <MUI/>
+    // <Header/> 
+    // <Student/>
+    // <Weather/>
+    <Task/>
+  )
+}
+export default App;
