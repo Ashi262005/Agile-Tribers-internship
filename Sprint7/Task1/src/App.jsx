b0f2786f6d2ebe3f5938591c414017195551aa82
Ashika A L <ashikaalexander4@gmail.com>
2025-05-23 13:51:56 +0530

    Add files via upload

---
+++ Sprint7/Task1/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './App.css'
+import { Task1 } from './Components/task1'
+import './Components/task1.css'
+import { Task2 } from './Components/task2'
+import './Components/task2.css'
+import { Task3 } from './Components/task3'
+import './Components/task3.css'
+import { Task4 } from './Components/task4'
+import './Components/task4.css'
+import { Task5 } from './Components/task5'
+import './Components/task5.css'
+import { Task6 } from './Components/task6'
+import './Components/task6.css'
+import { Task7 } from './Components/task7'
+import './Components/task7.css'
+import { Task8 } from './Components/task8'
+import './Components/task8.css'
+import { Task9 } from './Components/task9'
+import './Components/task9.css'
+function App() {
+ 
+
+  return (
+    <>
+      <Task1/> 
+      <Task2/>
+     <Task3/>
+      <Task4/>
+      <Task5/>
+      <Task6/> 
+     <Task7/> 
+      <Task8/>
+       <Task9/>
+    </>
+  )
+}
+
+export default App
